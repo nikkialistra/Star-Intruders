fab214e9cabdc127990b3d86801d08928a0709f4
nikkialistra <nikkialistra@gmail.com>
2021-11-12 23:15:20 +0500

    Add meteors

---
+++ Assets/Scripts/Environment/MeteorGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+    public class MeteorGenerator : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _meteors;
+
+        [SerializeField] private int _quantityToGenerate;
+        [SerializeField] private float _radius;
+        [SerializeField] private float _scaleRange;
+
+        private void Start()
+        {
+            var quantity = 0;
+            while (quantity < _quantityToGenerate)
+            {
+                var spawnPosition = new Vector3(Random.insideUnitSphere.x * _radius, 
+                    Random.insideUnitSphere.y * _radius, Random.insideUnitSphere.z * _radius);
+
+                if (spawnPosition.z > transform.position.z)
+                {
+                    var index = Random.Range(0, _meteors.Count);
+                    var scale = Random.Range(1, _scaleRange);
+
+                    var meteor = Instantiate(_meteors[index], spawnPosition, Random.rotation, transform);
+                    meteor.transform.localScale = new Vector3(scale, scale, scale);
+                    
+                    quantity++;
+                }
+            }
+            
+        }
+    }
+}
