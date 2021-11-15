c745ab307992389aa250a5c12de0fb7c93c91853
nikkialistra <nikkialistra@gmail.com>
2021-11-15 17:44:55 +0500

    Check asteroid generation for collisions

--- Assets/Scripts/Environment/AsteroidGenerator.cs
+++ Assets/Scripts/Environment/AsteroidGenerator.cs
@@ -10,27 +10,80 @@ namespace Environment
         [SerializeField] private int _amountToSpawn;
         [SerializeField] private float _minRange;
         [SerializeField] private float _maxRange;
-        [SerializeField] private float _scaleRange;
+        [SerializeField] private float _maxScale;
+        [SerializeField] private float _distanceBetweenAsteroidMultiplier;
 
         private void Start()
+        {
+            Generate();
+        }
+
+        private void Generate()
         {
             var quantity = 0;
             while (quantity < _amountToSpawn)
             {
-                var spawnPosition = transform.position + new Vector3(Random.insideUnitSphere.x * Random.Range(_minRange, _maxRange), 
-                    Random.insideUnitSphere.y * Random.Range(_minRange, _maxRange), Random.Range(_minRange, _maxRange));
+                var spawnPosition = GetSpawnPosition();
 
-                if (spawnPosition.z > transform.position.z)
+                if (IsPositionBeyondBase(spawnPosition))
                 {
-                    var index = Random.Range(0, _meteors.Count);
-                    var scale = Random.Range(1, _scaleRange);
-
-                    var meteor = Instantiate(_meteors[index], spawnPosition, Random.rotation, transform);
-                    meteor.transform.localScale = new Vector3(scale, scale, scale);
-                    
-                    quantity++;
+                    if (TryPlace(spawnPosition))
+                    {
+                        quantity++;
+                    }
                 }
             }
+        }
+
+        private Vector3 GetSpawnPosition()
+        {
+            var spawnPosition = transform.position + new Vector3(
+                Random.insideUnitSphere.x * Random.Range(_minRange, _maxRange),
+                Random.insideUnitSphere.y * Random.Range(_minRange, _maxRange), Random.Range(_minRange, _maxRange));
+            return spawnPosition;
+        }
+
+        private bool IsPositionBeyondBase(Vector3 spawnPosition)
+        {
+            return spawnPosition.z > transform.position.z;
+        }
+
+        private bool TryPlace(Vector3 spawnPosition)
+        {
+            var asteroid = InstantiateAsteroid(spawnPosition);
+
+            if (IsAnotherAsteroidClose(spawnPosition, asteroid))
+            {
+                Destroy(asteroid);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private GameObject InstantiateAsteroid(Vector3 spawnPosition)
+        {
+            var index = Random.Range(0, _meteors.Count);
+            var scale = Random.Range(1, _maxScale);
+
+            var asteroid = Instantiate(_meteors[index], spawnPosition, Random.rotation, transform);
+            asteroid.transform.localScale = new Vector3(scale, scale, scale);
+            return asteroid;
+        }
+
+        private bool IsAnotherAsteroidClose(Vector3 spawnPosition, GameObject asteroid)
+        {
+            var boxBounds = new Bounds(spawnPosition, asteroid.GetComponent<MeshCollider>().bounds.size);
+            var sqrHalfBoxSize = boxBounds.extents.sqrMagnitude;
+            var overlappingSphereRadius = Mathf.Sqrt(sqrHalfBoxSize + sqrHalfBoxSize);
+            var checkedDistance = overlappingSphereRadius * _distanceBetweenAsteroidMultiplier;
+
+            var results = new Collider[5];
+            var size = Physics.OverlapSphereNonAlloc(spawnPosition, checkedDistance, results);
+
+            return size > 1;
         }
     }
 }
